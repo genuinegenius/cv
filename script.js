64ec84dc9572f0698169c6c18e994888cee5b90f
genuinegenius <76172880+genuinegenius@users.noreply.github.com>
2021-10-22 14:41:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 document.getElementById("menuDots").addEventListener("click", () => {
     document.getElementById("content").style.display = "none";
     document.getElementById("contentNavClick").style.display = "block";
+    document.getElementById("menuDotsBack").style.display = "flex";
+})
 
+document.getElementById("menuDotsBack").addEventListener("click", () => {
+    document.getElementById("content").style.display = "block";
+    document.getElementById("contentNavClick").style.display = "none";
+    document.getElementById("menuDotsBack").style.display = "none";
 })
