27a1db0b7dff4f17e10d3f64f0b0ef721ab4af1c
genuinegenius <76172880+genuinegenius@users.noreply.github.com>
2021-10-22 16:16:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,35 @@ document.getElementById("menuDots").addEventListener("click", () => {
 })
 
 document.getElementById("menuDotsBack").addEventListener("click", () => {
-    document.getElementById("content").style.display = "block";
+    document.getElementById("content").style.display = "flex";
     document.getElementById("contentNavClick").style.display = "none";
     document.getElementById("menuDotsBack").style.display = "none";
+})
+
+document.getElementById("about_me_btn").addEventListener("click", () => {
+    document.getElementById("about_me_container").style.display = "flex";
+    document.getElementById("education_container").style.display = "none";
+    document.getElementById("skills_container").style.display = "none";
+    document.getElementById("projects_container").style.display = "none";
+})
+
+document.getElementById("education_btn").addEventListener("click", () => {
+    document.getElementById("about_me_container").style.display = "none";
+    document.getElementById("education_container").style.display = "flex";
+    document.getElementById("skills_container").style.display = "none";
+    document.getElementById("projects_container").style.display = "none";
+})
+
+document.getElementById("skills_btn").addEventListener("click", () => {
+    document.getElementById("about_me_container").style.display = "none";
+    document.getElementById("education_container").style.display = "none";
+    document.getElementById("skills_container").style.display = "flex";
+    document.getElementById("projects_container").style.display = "none";
+})
+
+document.getElementById("projects_btn").addEventListener("click", () => {
+    document.getElementById("about_me_container").style.display = "none";
+    document.getElementById("education_container").style.display = "none";
+    document.getElementById("skills_container").style.display = "none";
+    document.getElementById("projects_container").style.display = "flex";
 })
