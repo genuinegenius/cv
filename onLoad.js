8b527254f4e0a5739e602dce3e8ae17791162960
genuinegenius <76172880+genuinegenius@users.noreply.github.com>
2021-11-10 00:46:44 +0200

    Update onLoad.js

--- onLoad.js
+++ onLoad.js
@@ -19,4 +19,23 @@ document.addEventListener("DOMContentLoaded", () => {
         document.getElementById("contentNavClick").style.display = "none";
         document.getElementById("menuDotsBack").style.display = "none";
     }
+
+    if(width < 650){
+        document.getElementById("container_first_row").style.flexDirection = "column";
+        document.getElementById("container_second_row").style.flexDirection = "column";
+        document.getElementById("info1_1").style.width = "auto";
+        document.getElementById("info1_2").style.width = "auto";
+        document.getElementById("info1_3").style.width = "auto";
+        document.getElementById("info1_4").style.display = "none";
+        document.getElementById("tab").style.height = "auto";
+    }
+    else{
+        document.getElementById("container_first_row").style.flexDirection = "row";
+        document.getElementById("container_second_row").style.flexDirection = "row";
+        document.getElementById("info1_1").style.width = "30%";
+        document.getElementById("info1_2").style.width = "30%";
+        document.getElementById("info1_3").style.width = "30%";
+        document.getElementById("info1_4").style.display = "flex";
+        document.getElementById("tab").style.height = "450px";
+    }
 })
