b63d0b0b69772419c54349a8dd0e97bba10689cf
genuinegenius <76172880+genuinegenius@users.noreply.github.com>
2021-11-10 00:40:55 +0200

    Update widthScript.js

--- widthScript.js
+++ widthScript.js
@@ -20,6 +20,25 @@ function reportWindowSize() {
         document.getElementById("contentNavClick").style.display = "none";
         document.getElementById("menuDotsBack").style.display = "none";
     }
+
+    if(width < 410){
+        document.getElementById("container_first_row").style.flexDirection = "column";
+        document.getElementById("container_second_row").style.flexDirection = "column";
+        document.getElementById("info1_1").style.width = "auto";
+        document.getElementById("info1_2").style.width = "auto";
+        document.getElementById("info1_3").style.width = "auto";
+        document.getElementById("info1_4").style.display = "none";
+        document.getElementById("tab").style.height = "auto";
+    }
+    else{
+        document.getElementById("container_first_row").style.flexDirection = "row";
+        document.getElementById("container_second_row").style.flexDirection = "row";
+        document.getElementById("info1_1").style.width = "30%";
+        document.getElementById("info1_2").style.width = "30%";
+        document.getElementById("info1_3").style.width = "30%";
+        document.getElementById("info1_4").style.display = "flex";
+        document.getElementById("tab").style.height = "450px";
+    }
 }
 
 window.addEventListener('resize', reportWindowSize);
